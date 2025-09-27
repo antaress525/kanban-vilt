74804a4b8d5e51b38e5215f70a259101198775d4
antaress <nanatchendopjoelivan@gmail.com>
2025-09-28 00:16:07 +0100

    Refactor Forgot Password functionality: update UI text to French, enhance form structure, and implement validation rules

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,10 +1,10 @@
 <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, useForm } from '@inertiajs/vue3';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Input } from "@/components/ui/input";
+import { useForm } from '@inertiajs/vue3';
+import { Loader2 } from 'lucide-vue-next';
 
 defineProps({
     status: {
@@ -23,12 +23,11 @@ const submit = () => {
 
 <template>
     <GuestLayout>
-        <Head title="Forgot Password" />
+        <Head title="Mot de passe oublier" />
 
-        <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email
-            address and we will email you a password reset link that will allow
-            you to choose a new one.
+        <div class="text-center mb-4 sm:mb-6 md:mb-8">
+           <h1 class="text-2xl">Mot de passe oublié ?</h1>
+           <p class="text-sm text-neutral-400">Entrez votre adresse e-mail pour recevoir un lien de <br> réinitialisation</p>
         </div>
 
         <div
@@ -38,31 +37,17 @@ const submit = () => {
             {{ status }}
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
+        <form @submit.prevent="submit" class="space-y-6 w-full max-w-[340px]">
+            <!-- Email -->
+            <div class="space-y-2">
+                <Label for="email">Email</Label>
+                <Input type="email" id="email" v-model="form.email" placeholder="johhdoe@gmail.com" />
+                <label class="text-red-500 text-sm" v-if="form.errors.email">{{ form.errors.email }}</label>
             </div>
-
-            <div class="mt-4 flex items-center justify-end">
-                <PrimaryButton
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Email Password Reset Link
-                </PrimaryButton>
-            </div>
+            <Button class="w-full bg-black text-white">
+                <Loader2 v-if="form.processing" class="w-5 h-5 mr-2 animate-spin" />
+                Reinitialiser le mot de passe
+            </Button>
         </form>
     </GuestLayout>
 </template>
