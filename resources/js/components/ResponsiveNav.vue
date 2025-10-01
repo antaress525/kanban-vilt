df42de9a5aefb4044c82dab62902979428f89403
antaress <nanatchendopjoelivan@gmail.com>
2025-10-01 17:17:26 +0100

    feat: enhance AuthenticatedLayout with responsive navigation and improved layout structure

---
+++ resources/js/components/ResponsiveNav.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    <!-- Trigger -->
+    <button @click="toggle" class="space-y-1 w-5 rounded-md">
+        <div class="h-0.5  bg-black"></div>
+        <div class="h-0.5  bg-black"></div>
+        <div class="h-0.5  bg-black"></div>
+    </button>
+    <!-- Panel -->
+    <teleport to="body">
+        <div @click.self="close" v-if="open" class="fixed inset-0 bg-black/20">
+            <!-- Nav -->
+            <div class="h-full max-w-xs bg-white">
+                <!-- Header -->
+                <div class="flex px-2 items-center justify-between h-14 border-b border-neutral-100">
+                    <h3 class="text-xl font-medium">Kanban</h3>
+                    <Button @click="close" variant="ghost" size="icon">
+                        <X class="w-5 h-5" />
+                    </Button>
+                </div>
+                <!-- Content -->
+                <div class="h-full overflow-y-auto mt-8">
+                    <div class="space-y-6 px-2">
+                        <NavGroup label="Kanban">
+                            <template #nav-items>
+                                <NavItems as="button">
+                                    <Plus class="w-5 h-5" />
+                                    Creation kanban
+                                </NavItems>
+                            </template>
+                        </NavGroup>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </teleport>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Button } from '@/components/ui/button';
+import { X, Plus } from 'lucide-vue-next';
+import NavGroup from '@/components/NavGroup.vue';
+import NavItems from '@/components/NavItems.vue';
+import Profile from './Profile.vue';
+
+
+const open = ref(false);
+const toggle = () => {
+    open.value = !open.value;
+};
+const close = () => {
+    open.value = false;
+};
+</script>
